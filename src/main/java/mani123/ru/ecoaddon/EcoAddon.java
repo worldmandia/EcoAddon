9047a746e4e2dd888a5f17b78fa996e5034457de
mani123 <worldmandiareload@gmail.com>
2022-03-24 12:51:29 +0200

    Fixes

---
+++ src/main/java/mani123/ru/ecoaddon/EcoAddon.java
@@ -0,0 +1,17 @@
+package mani123.ru.ecoaddon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EcoAddon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
