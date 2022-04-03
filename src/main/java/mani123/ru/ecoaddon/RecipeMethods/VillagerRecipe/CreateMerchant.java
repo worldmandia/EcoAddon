953acf9341c73a4c9b2834ace178e4050dae5d8e
mani123 <worldmandiareload@gmail.com>
2022-04-03 21:14:41 +0300

    Fixes

---
+++ src/main/java/mani123/ru/ecoaddon/RecipeMethods/VillagerRecipe/CreateMerchant.java
@@ -0,0 +1,53 @@
+package mani123.ru.ecoaddon.RecipeMethods.VillagerRecipe;
+
+import com.willfp.eco.core.items.Items;
+import mani123.ru.ecoaddon.EcoAddon;
+import org.bukkit.Material;
+import org.bukkit.entity.Villager;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.MerchantRecipe;
+
+public class CreateMerchant {
+
+    public void CreateMerchantTrade(Villager event) {
+        if (EcoAddon.getPlugin().getConfigYml().getBool("enableVillagerTrades")) {
+            for (int i = 0; i < EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").size(); i++) {
+                double chance = EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getDouble("chance") / 100;
+                double randomChance = Math.random();
+                System.out.println("1");
+                if (randomChance <= chance) {
+                    String type = EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getString("type").toLowerCase().trim();
+                    System.out.println("2 " + event.getProfession().getKey().getKey() + "  "+ type);
+                    if (event.getProfession().getKey().getKey().equals(type)) {
+                        System.out.println("3");
+                        int level = EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("level") - 1;
+                        if (level <= event.getRecipes().size()) {
+                            System.out.println("4");
+                            System.out.println(level + " " + event.getRecipes().size());
+                            ItemStack result = Items.lookup(EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getFormattedString("result")).getItem();
+                            ItemStack inputOne = Items.lookup(EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getFormattedString("inputOne")).getItem();
+                            ItemStack inputTwo = Items.lookup(EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getFormattedString("inputTwo")).getItem();
+                            int uses = EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("uses");
+                            int maxUses = EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("maxUses");
+                            int villagerExperience = EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("villagerExperience");
+                            int demand = EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("demand");
+                            int specialPrice = EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("specialPrice");
+                            float priceMultiplier = (float) EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getDouble("priceMultiplier");
+                            boolean experienceReward = EcoAddon.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getBool("experienceReward");
+                            MerchantRecipe merchantRecipe = new MerchantRecipe(result, uses, maxUses, experienceReward, villagerExperience, priceMultiplier, demand, specialPrice);
+                            if (inputOne.getType() != Material.AIR) {
+                                merchantRecipe.addIngredient(inputOne);
+                            }
+                            if (inputTwo.getType() != Material.AIR) {
+                                merchantRecipe.addIngredient(inputTwo);
+                            }
+                            System.out.println("5");
+                            event.setRecipe(level, merchantRecipe);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
--- src/main/java/mani123/ru/ecoaddon/RecipeMethods/VillagerRecipe/VillagerListeners.java
+++ src/main/java/mani123/ru/ecoaddon/RecipeMethods/VillagerRecipe/VillagerListeners.java
@@ -1,15 +1,12 @@
 package mani123.ru.ecoaddon.RecipeMethods.VillagerRecipe;
 
 import com.willfp.eco.core.PluginDependent;
-import com.willfp.eco.core.items.Items;
 import mani123.ru.ecoaddon.EcoAddon;
-import org.bukkit.Material;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.VillagerAcquireTradeEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.event.entity.VillagerCareerChangeEvent;
+import org.bukkit.event.entity.VillagerReplenishTradeEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class VillagerListeners extends PluginDependent<EcoAddon> implements Listener {
@@ -18,40 +15,17 @@ public class VillagerListeners extends PluginDependent<EcoAddon> implements List
         super(plugin);
     }
 
+    CreateMerchant createMerchant = new CreateMerchant();
+
     @EventHandler
-    public void onEntitySpawnEvent(@NotNull final VillagerAcquireTradeEvent event) {
-        if (this.getPlugin().getConfigYml().getBool("enableStoneCutter")) {
-            for (int i = 0; i < this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").size(); i++) {
-                double chance = this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getDouble("chance") / 100;
-                double randomChance = Math.random();
-                if (randomChance <= chance) {
-                    String type = this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getString("type").toLowerCase().trim();
-                    Villager villager = (Villager) event.getEntity();
-                    if (villager.getProfession().getKey().getNamespace().equals("minecraft:" + type)) {
-                        int level = this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("level");
-                        if (villager.getVillagerLevel() == level || villager.getVillagerLevel() == 0) {
-                            ItemStack result = Items.lookup(this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getFormattedString("result")).getItem();
-                            ItemStack inputOne = Items.lookup(this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getFormattedString("inputOne")).getItem();
-                            ItemStack inputTwo = Items.lookup(this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getFormattedString("inputTwo")).getItem();
-                            int uses = this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("uses");
-                            int maxUses = this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("maxUses");
-                            int villagerExperience = this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("villagerExperience");
-                            int demand = this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("demand");
-                            int specialPrice = this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getInt("specialPrice");
-                            float priceMultiplier = (float) this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getDouble("priceMultiplier");
-                            boolean experienceReward = this.getPlugin().getCraftsYml().getSubsections("VillagerTrade").get(i).getBool("experienceReward");
-                            MerchantRecipe merchantRecipe = new MerchantRecipe(result, uses, maxUses, experienceReward, villagerExperience, priceMultiplier, demand, specialPrice);
-                            if (inputOne.getType() != Material.AIR) {
-                                merchantRecipe.addIngredient(inputOne);
-                            }
-                            if (inputTwo.getType() != Material.AIR) {
-                                merchantRecipe.addIngredient(inputTwo);
-                            }
-                            event.setRecipe(merchantRecipe);
-                        }
-                    }
-                }
-            }
-        }
+    public void onEntitySpawnEvent(@NotNull final VillagerReplenishTradeEvent event) {
+        createMerchant.CreateMerchantTrade((Villager) event.getEntity());
     }
+
+    @EventHandler
+    public void onEntitySpawnEvent(@NotNull final VillagerCareerChangeEvent event) {
+        createMerchant.CreateMerchantTrade(event.getEntity());
+    }
+
+
 }
