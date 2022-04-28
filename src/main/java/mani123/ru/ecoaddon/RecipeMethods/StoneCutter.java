b4cc74f4e071883f7d5352c8bb4322150fa01f18
mani123 <worldmandiareload@gmail.com>
2022-04-28 21:34:10 +0200

    Recoded StoneCutter

--- src/main/java/mani123/ru/ecoaddon/RecipeMethods/StoneCutter.java
+++ src/main/java/mani123/ru/ecoaddon/RecipeMethods/StoneCutter.java
@@ -1,5 +1,6 @@
 package mani123.ru.ecoaddon.RecipeMethods;
 
+import com.willfp.eco.core.config.interfaces.Config;
 import com.willfp.eco.core.items.Items;
 import com.willfp.eco.util.NamespacedKeyUtils;
 import mani123.ru.ecoaddon.EcoAddon;
@@ -14,37 +15,20 @@ import java.util.ArrayList;
 
 public class StoneCutter {
 
-    private static ArrayList<NamespacedKey> StoneCutterNamespace = new ArrayList<>();
+    private static final ArrayList<NamespacedKey> StoneCutterNamespace = new ArrayList<>();
 
     public static void StoneCutterListener(@NotNull final EcoAddon plugin) {
-        if (plugin.getConfigYml().getBool("enableStoneCutter")) {
-            for (int i = 0; i < plugin.getCraftsYml().getSubsections("StoneCutter").size(); i++) {
-                if (!Items.lookup(plugin.getCraftsYml().getSubsections("StoneCutter").get(i).getFormattedString("input")).matches(DefaultMethods.getAIR())
-                        && !Items.lookup(plugin.getCraftsYml().getSubsections("StoneCutter").get(i).getFormattedString("result")).matches(DefaultMethods.getAIR())) {
-                    ItemStack input = Items.lookup(plugin.getCraftsYml().getSubsections("StoneCutter").get(i).getFormattedString("input")).getItem();
-                    ItemStack result = Items.lookup(plugin.getCraftsYml().getSubsections("StoneCutter").get(i).getFormattedString("result")).getItem();
-                    String group = plugin.getCraftsYml().getSubsections("StoneCutter").get(i).getFormattedString("group");
-                    NamespacedKey namespacedKey = NamespacedKeyUtils.create("ecoaddon", plugin.getCraftsYml().getSubsections("StoneCutter").get(i).getString("id").toLowerCase().trim());
-                    StoneCutterMethod(namespacedKey, input, result, group);
-                }
-            }
+        if (!plugin.getConfigYml().getBool("enableStoneCutter")) return;
+        for (Config CfgSub : plugin.getCraftsYml().getSubsections("StoneCutter")) {
+            ItemStack input = Items.lookup(CfgSub.getFormattedString("input")).getItem();
+            ItemStack result = Items.lookup(CfgSub.getFormattedString("result")).getItem();
+            String group = CfgSub.getFormattedString("group");
+            NamespacedKey namespacedKey = NamespacedKeyUtils.create("ecoaddon", CfgSub.getFormattedString("id"));
+            StoneCutterNamespace.add(namespacedKey);
+            StonecuttingRecipe stonecuttingRecipe = new StonecuttingRecipe(namespacedKey, result, (RecipeChoice) input);
+            stonecuttingRecipe.setGroup(group);
+            Bukkit.addRecipe(stonecuttingRecipe);
         }
-    }
-
-    public static void StoneCutterMethod(@NotNull NamespacedKey key, @NotNull ItemStack input, @NotNull ItemStack result, String group) {
-        RecipeChoice inputChange = new RecipeChoice.ExactChoice(input);
-        StonecuttingRecipe recipe = new org.bukkit.inventory.StonecuttingRecipe(key, result, inputChange);
-        recipe.setGroup(group);
-        Bukkit.addRecipe(recipe);
-        StoneCutterNamespace.add(key);
-    }
-
-    public static CharSequence getCraftsCount() {
-        return String.valueOf(StoneCutterNamespace.size());
-    }
-
-    public static ArrayList<NamespacedKey> getCraftsNames() {
-        return StoneCutterNamespace;
     }
 
     public static void ClearRecipes() {
@@ -55,4 +39,3 @@ public class StoneCutter {
     }
 
 }
-
