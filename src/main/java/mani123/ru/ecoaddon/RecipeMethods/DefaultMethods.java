f6854fa7a19b522d3318f9e78270654d4a360b44
mani123 <worldmandiareload@gmail.com>
2022-04-28 21:56:00 +0200

    Update DefaultMethods

--- src/main/java/mani123/ru/ecoaddon/RecipeMethods/DefaultMethods.java
+++ src/main/java/mani123/ru/ecoaddon/RecipeMethods/DefaultMethods.java
@@ -1,47 +1,17 @@
 package mani123.ru.ecoaddon.RecipeMethods;
 
-import mani123.ru.ecoaddon.EcoAddon;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
+import com.willfp.eco.libs.jetbrains.annotations.NotNull;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class DefaultMethods {
 
-    public static String getAllCraftsCount() {
-        return String.valueOf(Integer.parseInt(FurnaceRecipe.getCraftsCount().toString())
-                + Integer.parseInt(StoneCutter.getCraftsCount().toString())
-                + Integer.parseInt(CampfireRecipe.getCraftsCount().toString())
-                + Integer.parseInt(SmokingRecipe.getCraftsCount().toString())
-                + Integer.parseInt(SmithingRecipe.getCraftsCount().toString())
-                + Integer.parseInt(BlastingRecipe.getCraftsCount().toString()));
-    }
-
-    public static String getAllCraftsNames() {
-        return FurnaceRecipe.getCraftsNames().toString()
-                + StoneCutter.getCraftsNames()
-                + SmokingRecipe.getCraftsNames()
-                + CampfireRecipe.getCraftsNames()
-                + SmithingRecipe.getCraftsNames()
-                + BlastingRecipe.getCraftsNames();
-    }
-
-    public static ItemStack getAIR() {
-        return new ItemStack(Material.AIR);
-    }
-
-    public static String getCraftsNamesList(String name) {
-        ArrayList<String> out = new ArrayList<>();
-        for (int i = 0; i < EcoAddon.getPlugin().getCraftsYml().getSubsections(name).size(); i++) {
-            out.add(EcoAddon.getPlugin().getCraftsYml().getSubsections(name).get(i).getString("id"));
+    public static void ClearCrafts(@NotNull ArrayList<NamespacedKey> items) {
+        for (NamespacedKey keys : items) {
+            Bukkit.removeRecipe(keys);
         }
-        return out.stream().map(Object::toString)
-                .collect(Collectors.joining(", "));
-    }
-
-    public static String getCraftsNamesListCount(String name) {
-        return String.valueOf(EcoAddon.getPlugin().getCraftsYml().getSubsections(name).size());
     }
 
 }
